cbedc63895fd40b48833ea614ffafc891f7e6089
feibuzhid <3476953365@qq.com>
2023-09-10 23:01:27 +0800

    helloworld first commit

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView tv_hello = findViewById(R.id.tv_hello);
+        tv_hello.setText("你好,世界!!!!!");
+    }
+}
