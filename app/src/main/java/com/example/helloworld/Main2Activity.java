0aa45ca5b790f5424089528486dd5acee7f93788
feibuzhid <3476953365@qq.com>
2023-09-12 00:07:04 +0800

    添加页面

---
+++ app/src/main/java/com/example/helloworld/Main2Activity.java
@@ -0,0 +1,19 @@
+package com.example.helloworld;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @author fei
+ * @since 2023/09/11 22:59
+ */
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
