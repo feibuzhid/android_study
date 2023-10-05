0896e70282db241c95c8fe287918337376af0557
feibuzhid <3476953365@qq.com>
2023-10-05 08:32:28 +0800

    自定义布局 temp

--- app/src/main/java/com/example/helloworld/Main3Activity.java
+++ app/src/main/java/com/example/helloworld/Main3Activity.java
@@ -2,7 +2,10 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main3Activity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class Main3Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        Button bt_jump2car = findViewById(R.id.bt_jump2car);
+        bt_jump2car.setOnClickListener(view -> {
+            startActivity(new Intent(this, CarActivity.class));
+        });
     }
+
+
+
 }
